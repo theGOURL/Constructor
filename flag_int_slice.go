2579a90cce969e5a717e103e25dd0f674589f761
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-03-03 14:29:37 -0300

    code

---
+++ flag_int_slice.go
@@ -0,0 +1,27 @@
+package cli
+
+import "flag"
+
+type IntSlice = SliceBase[int, IntegerConfig, intValue]
+type IntSliceFlag = FlagBase[[]int, IntegerConfig, IntSlice]
+
+var NewIntSlice = NewSliceBase[int, IntegerConfig, intValue]
+
+// IntSlice looks up the value of a local IntSliceFlag, returns
+// nil if not found
+func (cCtx *Context) IntSlice(name string) []int {
+	if fs := cCtx.lookupFlagSet(name); fs != nil {
+		return lookupIntSlice(name, fs)
+	}
+	return nil
+}
+
+func lookupIntSlice(name string, set *flag.FlagSet) []int {
+	f := set.Lookup(name)
+	if f != nil {
+		if slice, ok := f.Value.(flag.Getter).Get().([]int); ok {
+			return slice
+		}
+	}
+	return nil
+}
---
+++ flag_uint_slice.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"flag"
+)
+
+type UintSlice = SliceBase[uint, IntegerConfig, uintValue]
+type UintSliceFlag = FlagBase[[]uint, IntegerConfig, UintSlice]
+
+var NewUintSlice = NewSliceBase[uint, IntegerConfig, uintValue]
+
+// UintSlice looks up the value of a local UintSliceFlag, returns
+// nil if not found
+func (cCtx *Context) UintSlice(name string) []uint {
+	if fs := cCtx.lookupFlagSet(name); fs != nil {
+		return lookupUintSlice(name, fs)
+	}
+	return nil
+}
+
+func lookupUintSlice(name string, set *flag.FlagSet) []uint {
+	f := set.Lookup(name)
+	if f != nil {
+		if slice, ok := f.Value.(flag.Getter).Get().([]uint); ok {
+			return slice
+		}
+	}
+	return nil
+}
